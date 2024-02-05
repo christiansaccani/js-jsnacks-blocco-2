fa5265f8bd9f5ae48ec32cc7f77df293479faf34
christiansaccani <christian.saccani99@gmail.com>
2024-02-05 15:53:17 +0100

    snack 1 prova

--- Snack1/script.js
+++ Snack1/script.js
@@ -1,4 +1,25 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere 
 // i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
-let numberList = []
+let numberList = [];
+let contatore = 0;
+let sum = 0
+
+while (sum<50) {
+    const numberUser = +prompt("Inserisci un valore (valore totale massimo 50): ");
+
+    if( typeof numberUser === "number") {
+
+        
+        numberList.push(numberUser);
+        console.log(numberUser);
+        sum += numberUser;
+
+    } else {
+        alert("Il valore inserito non è valido");
+    
+    }
+}
+
+console.log(sum);
+document.querySelector("div").innerText = "Hai superato 50!"
